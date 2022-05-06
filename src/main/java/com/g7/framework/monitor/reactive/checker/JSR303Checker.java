c5639bd421be93842f3731695ae2c6590081db83
DreamYao <805229888@qq.com>
2022-05-06 20:21:09 +0800

    代码修改

--- src/main/java/com/g7/framework/monitor/reactive/checker/JSR303Checker.java
+++ src/main/java/com/g7/framework/monitor/reactive/checker/JSR303Checker.java
@@ -16,21 +16,27 @@ public class JSR303Checker {
     public static <T> Mono<T> check(T o) {
         // 通过jsr303规范的注解来校验参数
         return Mono.create(sink -> {
-            Set<ConstraintViolation<Object>> constraintViolations = ParamValidatorFactory.INSTANCE.getValidator()
-                    .validate(o);
-            JSR303CheckException exception = null;
-            if (constraintViolations != null && !constraintViolations.isEmpty()) {
-                exception = new JSR303CheckException();
-                for (ConstraintViolation<Object> constraintViolation : constraintViolations) {
-                    exception.addError(constraintViolation.getPropertyPath().toString(),
-                            constraintViolation.getMessage());
-                }
-            }
+            final JSR303CheckException exception = checkReturn(o);
             if (exception != null) {
                 sink.error(exception);
             } else {
                 sink.success(o);
             }
         });
+    }
+
+    public static <T> JSR303CheckException checkReturn(T o) {
+        Set<ConstraintViolation<Object>> constraintViolations = ParamValidatorFactory.INSTANCE.getValidator()
+                .validate(o);
+        JSR303CheckException exception = null;
+        if (constraintViolations != null && !constraintViolations.isEmpty()) {
+            exception = new JSR303CheckException();
+            for (ConstraintViolation<Object> constraintViolation : constraintViolations) {
+                exception.addError(constraintViolation.getPropertyPath().toString(),
+                        constraintViolation.getMessage());
+            }
+        }
+
+        return exception;
     }
 }
